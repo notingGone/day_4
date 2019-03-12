cc9fd2ff18d3031f0cbc11068a9a34e7c355c4a9
Elliott C. Woodward <elliottcwoodward@gmail.com>
2019-03-12 15:04:57 -0400

    uncommented all the code in _class.classes.rb and added instructions file

--- _class.classes.rb
+++ _class.classes.rb
@@ -26,64 +26,64 @@ single_pet = Pet.new("kangaroo", "Joey", 2, "boing")
 puts single_pet
 
 
-# my_pet = Pet.new("cat", "Gojira", 4, "meow")
-# my_pet.get_sound
-#
-# class Vehicle
-#
-#     attr_accessor :make, :model, :color, :mpg, :max_speed, :odometer
-#     def initialize
-#         @make = make
-#         @model = model
-#         @color = color
-#         @mpg = mpg
-#         @max_speed = max_speed
-#         @odometer = odometer
-#     end
-#
-#     def odometer_inc(num)
-#         @odometer += num
-#     end
-# end
-#
-#
-#
-# class Product
-#
-#     attr_accessor :name :product_type, :cost, :price, :quantity
-#
-#     def initialize(name, product_type, cost, price, quantity)
-#         @name = name
-#         @type = product_type
-#         @cost = cost
-#         @price = price
-#         @quantity = quantity
-#     end
-#
-#     def calc_profit_margin
-#      return @price - @cost
-#     end
-#
-#     def quantity_received(how_many)
-#         @quantity += how_many
-#     end
-#
-#     def quantity_sold(how_many)
-#         @quantity -= how_many
-#     end
-# end
-#
-# def get_product_info
-#     #gets input from user, database, etc
-# end
-#
-# i = 0
-# name, product_type, cost, price = get_prouct_info(i)
-# i += 1
-# first = Product.new(name, product_type, cost, price)
-#
-# name, product_type, cost, price = get_prouct_info(i)
-# i += 1
-# second = Product.new(name, product_type, cost, price)
-#
-# total_profit = first.calc_profit_margin + second.calc_profit_margin
+my_pet = Pet.new("cat", "Gojira", 4, "meow")
+my_pet.get_sound
+
+class Vehicle
+
+    attr_accessor :make, :model, :color, :mpg, :max_speed, :odometer
+    def initialize
+        @make = make
+        @model = model
+        @color = color
+        @mpg = mpg
+        @max_speed = max_speed
+        @odometer = odometer
+    end
+
+    def odometer_inc(num)
+        @odometer += num
+    end
+end
+
+
+
+class Product
+
+    attr_accessor :name :product_type, :cost, :price, :quantity
+
+    def initialize(name, product_type, cost, price, quantity)
+        @name = name
+        @type = product_type
+        @cost = cost
+        @price = price
+        @quantity = quantity
+    end
+
+    def calc_profit_margin
+     return @price - @cost
+    end
+
+    def quantity_received(how_many)
+        @quantity += how_many
+    end
+
+    def quantity_sold(how_many)
+        @quantity -= how_many
+    end
+end
+
+def get_product_info
+    #gets input from user, database, etc
+end
+
+i = 0
+name, product_type, cost, price = get_prouct_info(i)
+i += 1
+first = Product.new(name, product_type, cost, price)
+
+name, product_type, cost, price = get_prouct_info(i)
+i += 1
+second = Product.new(name, product_type, cost, price)
+
+total_profit = first.calc_profit_margin + second.calc_profit_margin
